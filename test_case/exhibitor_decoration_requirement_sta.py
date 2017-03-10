73a8106f4ff98899cf9e1961278fa1cf9097882d
linyang <linyang30@163.com>
2017-03-10 18:03:53 +0800

    login case

--- test_case/exhibitor_decoration_requirement_sta.py
+++ test_case/exhibitor_decoration_requirement_sta.py
@@ -4,4 +4,15 @@ from test_case.page_obj.exhibitor_back_page import Exhibitor_back
 class ExhibitionBackTest(myunit.MyTest):
 
     def test_exhibiton_back(self):
-        Exhibitor_back(self.driver).issue_decorating_requirement()
+        info = {
+            'title': '测试展会需求auto',
+            'exhibition_name': '测试2017011901',
+            'num': 1,
+            'service_type': '会展搭建',
+            'finish_time': '2017/03/30',
+            'design_req': '设计师需求',
+            'decorate_req': '展装需求'
+        }
+
+
+        Exhibitor_back(self.driver).issue_decorating_requirement(info)
--- test_case/page_obj/exhibitor_back_page.py
+++ test_case/page_obj/exhibitor_back_page.py
@@ -1,6 +1,9 @@
 from base import Page
 from login_page import Login
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+from selenium.webdriver.common.keys import Keys
 
 class Exhibitor_back(Page):
     '''参展商后台'''
@@ -9,7 +12,7 @@ class Exhibitor_back(Page):
     exhibitor_back_button_loc = (By.XPATH, '//*[@id="pg"]/div/ul/li[2]/a[2]')
 
     #发布展装需求
-    def issue_decorating_requirement(self, title, exhibition_name, num):
+    def issue_decorating_requirement(self, info):
         Login(self.driver).user_login(username='13500000018', password='123456')
         main_window = self.driver.current_window_handle
         self.find_element(*self.exhibitor_back_button_loc).click()
@@ -25,13 +28,32 @@ class Exhibitor_back(Page):
             if handle != main_window and handle != back_window:
                 self.driver.switch_to_window(handle)
         #填写标题
-        self.find_element(By.XPATH, '//*[@id="Subject"]').send_keys(title)
+        self.find_element(By.XPATH, '//*[@id="Subject"]').send_keys(info['title'])
         #填写展会名称
-        self.find_element(By.XPATH, '//*[@id="ExhibitionName"]').send_keys(exhibition_name)
+        self.find_element(By.XPATH, '//*[@id="ExhibitionName"]').send_keys(info['exhibition_name'])
+        time.sleep(2)
+        self.find_element(By.XPATH, '//*[@id="ExhibitionName"]').send_keys(Keys.ENTER)
         #填写展会编号
-        self.find_element(By.XPATH, '//*[@id="BoothName"]').send_keys(num)
-
-
-
+        self.find_element(By.XPATH, '//*[@id="BoothName"]').send_keys(info['num'])
+        time.sleep(2)
+        self.find_element(By.XPATH, '//*[@id="BoothName"]').send_keys(Keys.ENTER)
+        #选择服务类型
+        sel_service_type = self.find_element(By.ID, 'ServiceItem')
+        Select(sel_service_type).select_by_value(info['service_type'])
+        #选择展装公司地点
+        sel_province = self.find_element(By.ID, 'ProvinceCode')
+        Select(sel_province).select_by_value('440000')
+        sel_city = self.find_element(By.ID, 'CityCode')
+        Select(sel_city).select_by_value('440300')
+        #填写交工时间
+        js_finish_time = "$('input[name=FinishOn]').attr('value','%s')" % info['finish_time']
+        self.script(js_finish_time)
+        #填写设计师需求
+        self.find_element(By.ID, 'DesignerDemand').send_keys(info['design_req'])
+        #填写展装需求
+        self.find_element(By.ID, 'DecorateDemand').send_keys(info['decorate_req'])
+        #提交需求
+        self.find_element(By.ID, 'btnDecorateBookSave').click()
+        time.sleep(10)
 
 
